8bd8ee91f408ded8fbe91b375b086d3b4e87724c
imycod <317009160@qq.com>
2022-04-04 22:04:11 +0800

    CSS实现弹簧动画效果

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <div class="bd">
+    <div class="box">111</div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "node_modules/mathsass/dist/_math";
+
+.bd {
+  border: 1px solid #ccc;
+}
+
+@function spring-wobbly($t) {
+  @return -0.5 * pow(2.71828, (-6 * $t)) *
+    (-2 * pow(2.71828, (6 * $t)) + sin(12 * $t) + 2 * cos(12 * $t));
+}
+@function lerp($a, $b, $p) {
+  @return $a + $p * ($b - $a);
+}
+@keyframes move {
+  @for $i from 0 through 100 {
+    #{$i}% {
+      left: lerp(100px, 200px, spring-wobbly($i / 100));
+    }
+  }
+}
+.box {
+  animation: 1s move linear infinite;
+  position: relative;
+  background: red;
+  width: 100px;
+  height: 100px;
+}
+</style>
